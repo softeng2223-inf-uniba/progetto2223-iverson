b87d62e92e0fb7522fe1127b1d96be9ce14f764d
Riccardo Carichino <r.carichino@studenti.uniba.it>
2023-05-18 01:06:21 +0200

    Fine controllo e correzione di checkstyle

--- src/main/java/it/uniba/game/partita/Griglia.java
+++ src/main/java/it/uniba/game/partita/Griglia.java
@@ -1,6 +1,13 @@
 package it.uniba.game.partita;
 
-import it.uniba.game.nave.*;
+import it.uniba.game.nave.Cacciatorpediniere;
+import it.uniba.game.nave.Coordinata;
+import it.uniba.game.nave.Corazzata;
+import it.uniba.game.nave.Incrociatore;
+import it.uniba.game.nave.Nave;
+import it.uniba.game.nave.Portaerei;
+
+import it.uniba.game.utility.Dimensioni;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -8,33 +15,34 @@ import java.util.Random;
 import it.uniba.game.eccezioni.NumeroCoordinateException;
 
 /**
- * Classe che definisce la griglia di gioco e la inizializza
+ * Classe che definisce la griglia di gioco e la inizializza.
  */
 class Griglia {
     private int righe;
     private int colonne;
 
-    private String griglia[][];
+    private String[][] griglia;
     private ArrayList<ArrayList<Nave>> navi;
 
     private int numNavi;
 
     /**
-     * Inizializza la griglia di gioco
-     * 
-     * @param righe   : numero di righe della griglia
-     * @param colonne : numero di colonne della griglia
+     * Inizializza la griglia di gioco.
+     *
+     * @param pRighe   : numero di righe della griglia
+     * @param pColonne : numero di colonne della griglia
      * @throws NumeroCoordinateException se si inseriscono un numero diverso di
-     *                                   coordinate
-     *                                   rispetto alla sua dimensione
+     * coordinate rispetto alla sua dimensione
      */
-    public Griglia(int righe, int colonne) throws NumeroCoordinateException {
-        this.righe = righe;
-        this.colonne = colonne;
+    Griglia(final int pRighe, final int pColonne) throws NumeroCoordinateException {
+        this.righe = pRighe;
+        this.colonne = pColonne;
         this.griglia = new String[this.righe][this.colonne];
-        for (int i = 0; i < this.griglia.length; i++)
-            for (int j = 0; j < this.griglia.length; j++)
+        for (int i = 0; i < this.griglia.length; i++) {
+            for (int j = 0; j < this.griglia.length; j++) {
                 this.griglia[i][j] = new String("V");
+            }
+        }
 
         impostaNavi();
         this.numNavi = setnumnavi();
@@ -42,8 +50,8 @@ class Griglia {
     }
 
     /**
-     * Metodo che restituisce il numero di righe della griglia
-     * 
+     * Metodo che restituisce il numero di righe della griglia.
+     *
      * @return numero di righe
      */
     int getRighe() {
@@ -51,8 +59,8 @@ class Griglia {
     }
 
     /**
-     * Metodo che restituisce il numero di colonne della griglia
-     * 
+     * Metodo che restituisce il numero di colonne della griglia.
+     *
      * @return numero di colonne
      */
     int getColonne() {
@@ -62,12 +70,12 @@ class Griglia {
     /**
      * Restituisce un array di coordinate consecutive prendendo
      * in input la dimensione di tale array considerando che le posizioni
-     * siano disponibili
-     * 
+     * siano disponibili.
+     *
      * @param dimnave : dimensione della nave su cui trovare celle vuote
      * @return un array di coordinate consecutive
      */
-    private Coordinata[] trovaCellaVuota(int dimnave) {
+    private Coordinata[] trovaCellaVuota(final int dimnave) {
         Random rand = new Random();
         rand.setSeed(System.currentTimeMillis());
         int triga;
@@ -84,34 +92,34 @@ class Griglia {
             if ((dimnave + triga) < this.righe) {
                 boolean esito = true;
                 int i = 0;
-                while (i < dimnave && esito == true) {
+                while (i < dimnave && esito) {
                     if (!this.griglia[triga + i][tcolonna].equals("V")) {
                         esito = false;
                     }
                     i++;
                 }
-                if (esito == true) {
+                if (esito) {
                     esitoR = true;
                 }
             }
             boolean esitoC = false;
-            if ((dimnave + tcolonna) < this.colonne && esitoR == false) {
+            if ((dimnave + tcolonna) < this.colonne && !esitoR) {
                 boolean esito = true;
                 int i = 0;
-                while (i < dimnave && esito == true) {
+                while (i < dimnave && esito) {
                     if (!this.griglia[triga][tcolonna + i].equals("V")) {
                         esito = false;
                     }
                     i++;
                 }
-                if (esito == true) {
+                if (esito) {
                     esitoC = true;
                 }
             }
-            if (esitoR == true) {
+            if (esitoR) {
                 esitoTrovato = true;
                 int i = 0;
-                Coordinata coordinate[] = new Coordinata[dimnave];
+                Coordinata[] coordinate = new Coordinata[dimnave];
                 while (i < dimnave) {
                     this.griglia[triga + i][tcolonna] = "N";
                     coordinate[i] = new Coordinata(triga + i, tcolonna);
@@ -119,10 +127,10 @@ class Griglia {
                 }
                 return coordinate;
 
-            } else if (esitoC == true) {
+            } else if (esitoC) {
                 esitoTrovato = true;
                 int i = 0;
-                Coordinata coordinate[] = new Coordinata[dimnave];
+                Coordinata[] coordinate = new Coordinata[dimnave];
                 while (i < dimnave) {
                     this.griglia[triga][tcolonna + i] = "N";
                     coordinate[i] = new Coordinata(triga, tcolonna + i);
@@ -130,92 +138,103 @@ class Griglia {
                 }
                 return coordinate;
             }
-        } while (esitoTrovato == false);
+        } while (!esitoTrovato);
         return null;
     }
 
     /**
-     * Imposta la posizione della navi sulla Griglia casualmente
-     * 
+     * Imposta la posizione della navi sulla Griglia casualmente.
+     *
      * @throws NumeroCoordinateException se si inseriscono un numero diverso di
      *                                   coordinate
      *                                   rispetto alla sua dimensione
      */
     private void impostaNavi() throws NumeroCoordinateException {
         this.navi = new ArrayList<ArrayList<Nave>>();
-        for (int i = 0; i < 4; i++)
+        int tipiNavi = Dimensioni.values().length;
+        // Istanziamento dei tipi di navi
+        for (int i = 0; i < tipiNavi; i++) {
             this.navi.add(i, new ArrayList<Nave>());
+        }
 
-        for (int i = 0; i < 2; i++)
-            this.navi.get(0).add(new Cacciatorpediniere(trovaCellaVuota(2)));
-
-        for (int i = 0; i < 3; i++)
-            this.navi.get(1).add(new Incrociatore(trovaCellaVuota(3)));
-
-        for (int i = 0; i < 2; i++)
-            this.navi.get(2).add(new Corazzata(trovaCellaVuota(4)));
-
-        for (int i = 0; i < 1; i++)
-            this.navi.get(3).add(new Portaerei(trovaCellaVuota(5)));
+        //Istanziamento delle navi di ogni tipo
+        for (int i = 0; i < Dimensioni.valueOf("CACCIATORPEDINIERE").getEsemplari(); i++) {
+            this.navi.get(Dimensioni.valueOf("CACCIATORPEDINIERE").getIndex()).add(new Cacciatorpediniere(
+                trovaCellaVuota(Dimensioni.valueOf("CACCIATORPEDINIERE").getDim())));
+        }
+        for (int i = 0; i < Dimensioni.valueOf("INCROCIATORE").getEsemplari(); i++) {
+            this.navi.get(Dimensioni.valueOf("INCROCIATORE").getIndex()).add(new Incrociatore(
+                trovaCellaVuota(Dimensioni.valueOf("INCROCIATORE").getDim())));
+        }
+        for (int i = 0; i < Dimensioni.valueOf("CORAZZATA").getEsemplari(); i++) {
+            this.navi.get(Dimensioni.valueOf("CORAZZATA").getIndex()).add(new Corazzata(
+                trovaCellaVuota(Dimensioni.valueOf("CORAZZATA").getDim())));
+        }
+        for (int i = 0; i < Dimensioni.valueOf("PORTAEREI").getEsemplari(); i++) {
+            this.navi.get(Dimensioni.valueOf("PORTAEREI").getIndex()).add(new Portaerei(
+                trovaCellaVuota(Dimensioni.valueOf("PORTAEREI").getDim())));
+        }
 
     }
 
     /**
-     * Imposta numero delle navi
-     * 
-     * @return numero delle navi
+     * Imposta numero delle navi.
+     *
+     * @return pNumNavi numero delle navi
      */
     private int setnumnavi() {
-        int numNavi = 0;
-        for (int i = 0; i < this.navi.size(); i++)
-            for (int j = 0; j < this.navi.get(i).size(); j++)
-                numNavi = numNavi + this.navi.get(j).size();
-        return numNavi;
+        int pNumNavi = 0;
+        for (int i = 0; i < this.navi.size(); i++) {
+            for (int j = 0; j < this.navi.get(i).size(); j++) {
+                pNumNavi = pNumNavi + this.navi.get(j).size();
+            }
+        }
+        return pNumNavi;
 
     }
 
     /**
-     * Descrive lo stato della Griglia
-     * 
+     * Descrive lo stato della Griglia.
+     *
      * @return stato di Griglia
      */
     public String toString() {
-        StringBuilder griglia = new StringBuilder("");
+        StringBuilder strGriglia = new StringBuilder("");
         for (int i = 0; i < this.griglia.length; i++) {
             for (int j = 0; j < this.griglia[i].length; j++) {
-                griglia.append(this.griglia[i][j] + " ");
+                strGriglia.append(this.griglia[i][j] + " ");
             }
-            griglia.append("\n");
+            strGriglia.append("\n");
         }
-        return griglia.toString();
+        return strGriglia.toString();
     }
 
     /**
-     * Restituisce vero se tutte le navi sono state abbattute
-     * 
+     * Restituisce vero se tutte le navi sono state abbattute.
+     *
      * @return vero se tutte le navi sono state abbattute
      */
     boolean finepartita() {
-        return false;
+        return true;
     }
 
     /**
-     * Inserisce il colpo alla posizione inserita in input
-     * 
+     * Inserisce il colpo alla posizione inserita in input.
+     *
      * @param riga    : riga su cui si inserisce il colpo
      * @param colonna : colonna su cui inserisce il colpo
      */
-    void inserisciColpo(int riga, int colonna) {
+    void inserisciColpo(final int riga, final int colonna) {
     }
 
     /**
-     * Restituisce una cella della Griglia
-     * 
+     * Restituisce una cella della Griglia.
+     *
      * @param i : numero di riga della cella della griglia da restituire
      * @param j : numero di colonna della cella della griglia da restituire
      * @return singola cella della Griglia
      */
-    public String getCella(int i, int j) {
+    public String getCella(final int i, final int j) {
         return griglia[i][j];
     }
 
--- src/main/java/it/uniba/game/utility/Dimensioni.java
+++ src/main/java/it/uniba/game/utility/Dimensioni.java
@@ -3,20 +3,25 @@ package it.uniba.game.utility;
  * Classe enum che rappresenta i valori di dimensioni.
  */
 public enum Dimensioni {
-    CACCIATORPEDINIERE(2,4),
-    CORAZZATA(4,3),
-    INCROCIATORE(3,2),
-    PORTAEREI(5,1);
+    CACCIATORPEDINIERE(0, 2, 4),
+    CORAZZATA(1, 4, 3),
+    INCROCIATORE(2, 3, 2),
+    PORTAEREI(3, 5, 1);
 
     private int dim;
     private int esemplari;
+    private int index;
      /**
      * Costruttore della classe enum.
-     * @param pValue Puó essere una dimensione valida.
+     * @param pIndex Indica un indice con la quale si riferisce al numero che contraddistringue
+     * la nave in fase di inserimento
+     * @param pDim Puó essere una dimensione valida.
+     * @param pEsemplari Può essere un numero di esemplari.
      */
-    Dimensioni(final int pDim, final int pEsemplari) {
+    Dimensioni(final int pIndex, final int pDim, final int pEsemplari) {
         this.dim = pDim;
         this.esemplari = pEsemplari;
+        this.index = pIndex;
     }
 
     /**
@@ -34,4 +39,12 @@ public enum Dimensioni {
     public int getEsemplari() {
             return esemplari;
         }
+
+    /**
+     * Funzione che restituisce l'indice della nave.
+     * @return index il valore dell'indice della nave.
+     */
+    public int getIndex() {
+        return index;
+    }
 }
